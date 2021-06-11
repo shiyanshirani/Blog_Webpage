9decd088383e3103f1e4d753fc933d8e18b3a21b
Shiyan Shirani <shiyan@Shiyans-MacBook-Pro.local>
2021-06-11 21:27:55 +0530

    update post added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import LoginForm, RegisterationForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -100,4 +100,20 @@ def new_post():
         db.session.commit()
         flash('Your post has been created', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post',
+                                form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update")
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    return render_template ('create_post.html', title='Update Post',
+                                    form=form, legend='Update Post') 
