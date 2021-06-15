fa34ef0f88e77757fd445e669988113800720fb0
Shiyan Shirani <shiyan99s@gmail.com>
2021-06-15 08:21:51 +0530

    pagination applied

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -10,7 +10,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 # Route which will display home page.
 @app.route("/")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 # Route to display about section of the webpage
@@ -131,3 +132,13 @@ def update_post(post_id):
     return render_template('create_post.html', title='Update Post',
                             form=form, legend='Update Post')
 
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted.', 'success')
+    return redirect(url_for('home'))
